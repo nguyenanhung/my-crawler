053ecd72b360486421c0a6b6a5e54e2b4fb78aa0
nguyenanhung <info@nguyenanhung.com>
2023-08-23 16:04:31 +0700

    Releases

--- src/HttpBrowser.php
+++ src/HttpBrowser.php
@@ -4,12 +4,12 @@
  * Created by PhpStorm
  * User: 713uk13m <dev@nguyenanhung.com>
  * Copyright: 713uk13m <dev@nguyenanhung.com>
- * Date: 14/08/2022
- * Time: 03:29
+ * Date: 23/08/2023
+ * Time: 16:00
  */
 
 namespace nguyenanhung\Libraries\Crawler;
 
-class Client extends HttpBrowser
+class HttpBrowser extends \Symfony\Component\BrowserKit\HttpBrowser
 {
 }
