3d4f0281c7c0ed7476eca67cd860c6b42f092d78
nguyenanhung <info@nguyenanhung.com>
2023-09-09 23:35:41 +0700

    Update Project at Sat Sep  9 23:35:41 +07 2023

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -1,1 +1,18 @@
 <?php
+if (!function_exists('_crawler_convert_youtube_embed_from_id_')) {
+    function _crawler_convert_youtube_embed_from_id_($youtubeID = ''): string
+    {
+        if (empty($youtubeID)) {
+            return '';
+        }
+
+        $youtubeID = str_replace('https://youtu.be/', '', $youtubeID);
+        $youtubeID = str_replace('https://www.youtube.com/watch?v=', '', $youtubeID);
+        $youtubeID = str_replace('https://www.youtube.com/embed/', '', $youtubeID);
+        $newHtml = '<div class="news-posts-content-youtube-video" style="text-align: center;">';
+        $newHtml .= '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . trim($youtubeID) . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+        $newHtml .= '</div>';
+
+        return trim($newHtml);
+    }
+}
--- src/CrawlerFilterTrait.php
+++ src/CrawlerFilterTrait.php
@@ -366,12 +366,7 @@ trait CrawlerFilterTrait
         foreach ($videoList as $oldItemHtml) {
             $oldItemHtml = trim($oldItemHtml);
             $youtubeID = $this->parseGetContentValueWithExplode($oldItemHtml, $matchVideoOpenTag, $matchVideoCloseTag);
-            $youtubeID = str_replace('https://youtu.be/', '', $youtubeID);
-            $youtubeID = str_replace('https://www.youtube.com/watch?v=', '', $youtubeID);
-            $youtubeID = str_replace('https://www.youtube.com/embed/', '', $youtubeID);
-            $newHtml = '<div class="news-posts-content-youtube-video" style="text-align: center;">';
-            $newHtml .= '<iframe width="560" height="315" src="https://www.youtube.com/embed/' . trim($youtubeID) . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-            $newHtml .= '</div>';
+            $newHtml = _crawler_convert_youtube_embed_from_id_($youtubeID);
             $content = str_replace($oldItemHtml, $newHtml, $content);
         }
         return $content;
