5dbc652bba12c01738a411f47b4b3057bbcd1415
nguyenanhung <info@nguyenanhung.com>
2023-09-27 11:52:49 +0700

    Update Project at Wed Sep 27 11:52:49 +07 2023

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -16,7 +16,7 @@ if (!function_exists('_crawler_convert_youtube_embed_from_id_')) {
             $youtubeID
         );
         $newHtml = '<div class="news-posts-content-youtube-video" style="padding-top: 10px;text-align: center;padding-bottom: 10px;">';
-        $newHtml .= '<iframe width="100%" height="445" src="https://www.youtube.com/embed/' . trim($youtubeID) . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+        $newHtml .= '<iframe loading="lazy" width="100%" height="445" src="https://www.youtube.com/embed/' . trim($youtubeID) . '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
         $newHtml .= '</div>';
         return trim($newHtml);
     }
@@ -24,7 +24,7 @@ if (!function_exists('_crawler_convert_youtube_embed_from_id_')) {
 if (!function_exists('_crawler_convert_image_src_from_url_')) {
     function _crawler_convert_image_src_from_url_($imgSrc = '', $title = '', $alt = ''): string
     {
-        $newImgSrc = '<img class="news-posts-content-image" width="100%" src="' . trim($imgSrc) . '" title="' . trim($title) . '" alt="' . trim($alt) . '" />';
+        $newImgSrc = '<img loading="lazy" class="news-posts-content-image" width="100%" src="' . trim($imgSrc) . '" title="' . trim($title) . '" alt="' . trim($alt) . '" />';
         return trim($newImgSrc);
     }
 }
